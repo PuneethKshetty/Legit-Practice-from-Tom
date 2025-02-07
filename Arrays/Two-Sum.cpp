12134018485218215e192eb018cd918dc0bc649c
X-Cyborg <puneethkshetty6720@gmail.com>
2025-02-07 12:01:06 +0530

    Update Two-Sum.cpp

--- Arrays/Two-Sum.cpp
+++ Arrays/Two-Sum.cpp
@@ -8,7 +8,7 @@ vector<int> twoSum(vector<int> &nums, int target)
     vector<int> res;
     for(int i=0;i<nums.size();i++)
     {
-        if(mp.find(target - nums[i]) ! = mp.end())
+        if(mp.find(target - nums[i]) != mp.end())
         {
            res.push_back(mp[target - nums[i]]);
            res.push_back(i);
