8e5921daa4596fc04b05b2fa5ea588b03eab020c
X-Cyborg <puneethkshetty6720@gmail.com>
2025-02-07 12:05:31 +0530

    Brute force method and optimized method added

--- Arrays/Two-Sum.cpp
+++ Arrays/Two-Sum.cpp
@@ -1,5 +1,21 @@
 #Two sum problem
 
+Brute force:
+#include<bits/stdc++.h>
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    for(int i=0;i<nums.size();i++)
+        for(int j = i+1;j<nums.size();j++)
+            if(target == nums[i] + nums[j])
+            {
+                return {i,j};
+            }
+    return {};  
+}
+
+
+Optimized approach:
 #include<bits/stdc++.h>
 
 vector<int> twoSum(vector<int> &nums, int target)
